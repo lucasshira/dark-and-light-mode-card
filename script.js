426ca7002013cf1f4302f058423e5199cfc9907e
Lucas Gabriel <144144880+lucasshira@users.noreply.github.com>
2023-10-14 16:22:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,28 @@ function toggleDarkMode() {
 
     const isDarkModeEnabled = document.body.classList.contains('dark-mode');
     localStorage.setItem('darkmode', isDarkModeEnabled);
+
+    document.title = isDarkModeEnabled ? 'Dark Mode' : 'Light Mode';
 }
 
 toggle.addEventListener('click', toggleDarkMode);
+
+toggle.addEventListener('mouseenter', () => {
+    const isDarkModeEnabled = document.body.classList.contains('dark-mode');
+    toggle.setAttribute('title', isDarkModeEnabled ? 'Change to Light Mode' : 'Change to Dark Mode');
+})
 
 window.addEventListener('load', () => {
     const isDarkModeEnabled = localStorage.getItem('darkmode');
 
     if(isDarkModeEnabled === 'true') {
-        document.body.classList.add('dark-mode')
+        document.body.classList.add('dark-mode');
+        document.title = 'Dark Mode';
+        toggle.setAttribute('title', 'Change to Light Mode');
     } else {
         document.body.classList.remove('dark-mode');
+        document.title = 'Light Mode';
+        toggle.setAttribute('title', 'Change to Dark Mode');
     }
 
     setInterval(() => {
